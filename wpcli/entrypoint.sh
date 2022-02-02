82dbfd92ebbd471e0d0f7a7eee368c3281497530
AIRoman <airoman@airoman.ro>
2022-02-02 15:21:52 +0200

    update wpcli

--- wpcli/entrypoint.sh
+++ wpcli/entrypoint.sh
@@ -6,14 +6,14 @@ set -euo pipefail
 
 # If the first arg is `--some-option` then execute wp-cli
 if [ "${1#-}" != "$1" ]; then
-	set -- wp "$@"
+	set -- wp-cli "$@"
 fi
 
 # if our command is a valid wp-cli subcommand (say plugin), let's invoke it through wp-cli instead
 # (this allows for "docker run wordpress:cli help", etc)
-# documenation of the subcommand is shown
-if wp --path=/dev/null help "$1" > /dev/null 2>&1; then
-	set -- wp "$@"
+# documentation of the subcommand is shown
+if wp-cli --path=/dev/null help "$1" > /dev/null 2>&1; then
+	set -- wp-cli "$@"
 fi
 
 # Execute aliases in the make file or directly the provided command
